3d8901193e7ac9940a502af866e915af473a8350
mahehehes <mahestifajar@gmail.com>
2024-01-22 13:57:18 +0800

    belajar js

---
+++ praktek3/script.js
@@ -0,0 +1,7 @@
+const negara = "central african republic";
+const benua = "africa";
+let populasi = 5.457;
+const kepulauan = true;
+var bahasa = "Bahasa Perancis";
+
+console.log(`Negara ${negara}, adalah negara yang berada di benua ${benua}, dengan populasi sebanyak ${populasi} jiwa, bahasa nasional ${bahasa}`);
---
+++ praktek4/script.js
@@ -0,0 +1,17 @@
+const berat_Madan = 60;
+const tinggi_Madan = 1.6;
+
+const berat_Joan = 64;
+const tinggi_Joan = 1.65;
+
+const imtMadan = berat_Madan / tinggi_Madan ** 2;
+const imtJoan = berat_Joan / tinggi_Joan ** 2;
+
+
+if ( imtMadan > imtJoan) {
+    console.log(`Nilai IMT Madan ${imtMadan.toFixed(1)} Lebih Tinggi Dari Joan ${imtJoan.toFixed(1)}`);
+} else {
+    console.log(`Nilai IMT Joan ${imtJoan.toFixed(1)} Lebih Tinggi Dari Madan ${imtMadan.toFixed(1)}`);
+}
+
+
